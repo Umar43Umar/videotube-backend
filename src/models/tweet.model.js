86bf8c29daa91a9cd72a529f7177da3c091516ff
Umar43Umar <umar43umar@gmail.com>
2024-06-13 18:37:56 +0500

    making remaining models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const tweetSchema = new Schema({
+  owner:{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  content: {
+    type: String,
+    required: true
+  }
+}, {timestamps: true})
+
+export const Tweet = mongoose.model("Tweet", tweetSchema)
